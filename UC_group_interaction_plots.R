86483eb1a3589c1b77d9690421d5e670fb592765
KristyS <kristys@alumni.ubc.ca>
2019-08-13 14:26:34 -0700

    updated way of doing splitting

--- UC_group_interaction_plots.R
+++ UC_group_interaction_plots.R
@@ -25,13 +25,22 @@ combined_matrix$aoi_name <- gsub(' |-', '_', combined_matrix$aoi_name)
 
 setDT(combined_matrix)[timetofirstfixation == -1, timetofirstfixation := 300000]
 
+ctrl_bcl_raw <- unique(fread("mmd_study1_barchartlit_raw.csv", sep=",")[, c('user_id', 'BarChartLit_raw')])
+setnames(ctrl_bcl_raw, 1, 'part_id')
+ctrl_bcl_raw[, part_id := as.character(part_id)]
+adpt_bcl_raw <- fread("mmd_study2_barchartlit_raw.csv", sep=",")[, c('user_id', 'BarChartLit_raw')]
+setnames(adpt_bcl_raw, 1, 'part_id')
+bcl_raw <- rbind(ctrl_bcl_raw, adpt_bcl_raw)
+combined_matrix <- merge(combined_matrix, bcl_raw, all.x = TRUE)
+
+uc_subset <- combined_matrix[, .SD[1], by=part_id, .SDcols=c('Meara', 'BarChartLit_raw')]
 
 # 2-way split
-combined_matrix[Meara>=median(Meara), Meara2 := 'High']
-combined_matrix[Meara<median(Meara), Meara2 := 'Low']
+combined_matrix[Meara>=median(uc_subset$Meara), Meara2 := 'High']
+combined_matrix[Meara<median(uc_subset$Meara), Meara2 := 'Low']
 combined_matrix[, Meara2 := factor(Meara2, levels=c('Low', 'High'))]
-combined_matrix[BarChartLit>=median(BarChartLit), VisLit2 := 'High']
-combined_matrix[BarChartLit<median(BarChartLit), VisLit2 := 'Low']
+combined_matrix[BarChartLit_raw>=median(uc_subset$BarChartLit_raw), VisLit2 := 'High']
+combined_matrix[BarChartLit_raw<median(uc_subset$BarChartLit_raw), VisLit2 := 'Low']
 combined_matrix[, VisLit2 := factor(VisLit2, levels=c('Low', 'High'))]
 
 
@@ -70,31 +79,47 @@ theme_update(plot.title = element_text(hjust = 0.5))
 
 sss <- summarySE(combined_matrix[aoi_name=='Relevant_bars'], measurevar="numtransfrom_Refs", groupvars=c("Meara2","group"))
 ggplot(sss, aes(x=Meara2, y=numtransfrom_Refs, group=group)) + 
-  geom_line(aes(color=group)) +
   geom_point(aes(color=group)) + 
-  geom_errorbar(aes(ymin=numtransfrom_Refs-ci*0.8, ymax=numtransfrom_Refs+ci*0.8, color=group), width=.1) +
-  ggtitle('Relevant_bars numtransfrom_Refs')
+  geom_errorbar(aes(ymin=numtransfrom_Refs-ci*0.8, ymax=numtransfrom_Refs+ci*0.8, color=group), width=.2, 
+                position = position_dodge(width = 0.08), size = 0.6, show.legend=FALSE) +
+  geom_line(aes(color=group, linetype=group), size = 0.75) +
+  theme(legend.key.size =  unit(0.5, "in")) +
+  scale_x_discrete(labels=c("Low", "High")) + 
+  labs(x = 'Reading Proficiency', y = "transitions from Refs to R-Bars") + 
+  scale_linetype_manual(values=c("dashed",'solid'))
 
 sss <- summarySE(combined_matrix[aoi_name=='Relevant_bars'], measurevar="numtransfrom_labels", groupvars=c("VisLit2","group"))
 ggplot(sss, aes(x=VisLit2, y=numtransfrom_labels, group=group)) + 
-  geom_line(aes(color=group)) +
+  geom_errorbar(aes(ymin=numtransfrom_labels-ci*0.8, ymax=numtransfrom_labels+ci*0.8, color=group), width=.2, 
+                position = position_dodge(width = 0.08), size = 0.6, show.legend=FALSE) +
   geom_point(aes(color=group)) + 
-  geom_errorbar(aes(ymin=numtransfrom_labels-ci*0.8, ymax=numtransfrom_labels+ci*0.8, color=group), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('Relevant_bars numtransfrom_labels')
+  geom_line(aes(color=group, linetype=group), size = 0.75) +
+  theme(legend.key.size =  unit(0.5, "in")) +
+  scale_x_discrete(labels=c("Low", "High")) + 
+  labs(x = 'Vis Literacy', y = "transitions from Labels to R-Bars") + 
+  scale_linetype_manual(values=c("dashed",'solid'))
 
 sss <- summarySE(combined_matrix[aoi_name=='legend'], measurevar="numtransfrom_Non_relevant_bars", groupvars=c("VisLit2","group"))
 ggplot(sss, aes(x=VisLit2, y=numtransfrom_Non_relevant_bars, group=group)) + 
-  geom_line(aes(color=group)) +
+  geom_errorbar(aes(ymin=numtransfrom_Non_relevant_bars-ci*0.8, ymax=numtransfrom_Non_relevant_bars+ci*0.8, color=group), width=.2, 
+                position = position_dodge(width = 0.08), size = 0.6, show.legend=FALSE) +
   geom_point(aes(color=group)) + 
-  geom_errorbar(aes(ymin=numtransfrom_Non_relevant_bars-ci*0.8, ymax=numtransfrom_Non_relevant_bars+ci*0.8, color=group), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('legend numtransfrom_Non_relevant_bars')
+  geom_line(aes(color=group, linetype=group), size = 0.75) +
+  theme(legend.key.size =  unit(0.5, "in")) +
+  scale_x_discrete(labels=c("Low", "High")) + 
+  labs(x = 'Vis Literacy', y = "transitions from NR-Bars to Legend") + 
+  scale_linetype_manual(values=c("dashed",'solid'))
 
 sss <- summarySE(combined_matrix[aoi_name=='legend'], measurevar="numtransfrom_Refs", groupvars=c("VisLit2","group"))
 ggplot(sss, aes(x=VisLit2, y=numtransfrom_Refs, group=group)) + 
-  geom_line(aes(color=group)) +
+  geom_errorbar(aes(ymin=numtransfrom_Refs-ci*0.8, ymax=numtransfrom_Refs+ci*0.8, color=group), width=.2, 
+                position = position_dodge(width = 0.08), size = 0.6, show.legend=FALSE) +
   geom_point(aes(color=group)) + 
-  geom_errorbar(aes(ymin=numtransfrom_Refs-ci*0.8, ymax=numtransfrom_Refs+ci*0.8, color=group), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('legend numtransfrom_Refs')
+  geom_line(aes(color=group, linetype=group), size = 0.75) +
+  theme(legend.key.size =  unit(0.5, "in")) +
+  scale_x_discrete(labels=c("Low", "High")) + 
+  labs(x = 'Vis Literacy', y = "transitions from Refs to Legend") + 
+  scale_linetype_manual(values=c("dashed",'solid'))
 
 #### reversed x and y 
 sss <- summarySE(combined_matrix[aoi_name=='Relevant_bars'], measurevar="numtransfrom_Refs", groupvars=c("Meara2","group"))
@@ -127,31 +152,19 @@ ggplot(sss, aes(x=group, y=numtransfrom_Refs, group=VisLit2)) +
 
 
 # 3-way split
-ctrl_bcl_raw <- unique(fread("mmd_study1_barchartlit_raw.csv", sep=",")[, c('user_id', 'BarChartLit_raw')])
-setnames(ctrl_bcl_raw, 1, 'part_id')
-ctrl_bcl_raw[, part_id := as.character(part_id)]
-adpt_bcl_raw <- fread("mmd_study2_barchartlit_raw.csv", sep=",")[, c('user_id', 'BarChartLit_raw')]
-setnames(adpt_bcl_raw, 1, 'part_id')
-bcl_raw <- rbind(ctrl_bcl_raw, adpt_bcl_raw)
 
-combined_matrix <- combined_matrix[order(BarChartLit)]
-bcl_raw <- bcl_raw[order(BarChartLit_raw)]
-pivot_lowA <- combined_matrix[nrow(combined_matrix)*(1/3)]$BarChartLit
-pivot_lowB <- bcl_raw[nrow(bcl_raw)*(1/3)]$BarChartLit_raw
-pivot_highA <- combined_matrix[nrow(combined_matrix)*(2/3)]$BarChartLit
-pivot_highB <- bcl_raw[nrow(bcl_raw)*(2/3)]$BarChartLit_raw
+uc_subset <- uc_subset[order(BarChartLit_raw)]
+pivot_low <- uc_subset[nrow(uc_subset)*(1/3)]$BarChartLit_raw
+pivot_high <- uc_subset[nrow(uc_subset)*(2/3)]$BarChartLit_raw
 
-combined_matrix[BarChartLit<pivot_lowA, VisLit3 := 'Low']
-combined_matrix[BarChartLit==pivot_lowA & BarChartLit<pivot_lowB, VisLit3 := 'Low']
-combined_matrix[BarChartLit<pivot_highA & BarChartLit>pivot_lowA, VisLit3 := 'Med']
-combined_matrix[BarChartLit==pivot_lowA & BarChartLit>=pivot_lowB, VisLit3 := 'Med']
-combined_matrix[BarChartLit>=pivot_highA, VisLit3 := 'High']
+combined_matrix[BarChartLit_raw<pivot_low, VisLit3 := 'Low']
+combined_matrix[BarChartLit_raw>=pivot_low & BarChartLit_raw<pivot_high, VisLit3 := 'Med']
+combined_matrix[BarChartLit_raw>=pivot_high, VisLit3 := 'High']
 combined_matrix[, VisLit3 := factor(VisLit3, levels=c('Low', 'Med', 'High'))]
 
-
-combined_matrix <- combined_matrix[order(Meara)]
-pivot_low <- combined_matrix[nrow(combined_matrix)*(1/4)]$Meara
-pivot_high <- combined_matrix[nrow(combined_matrix)*(3/4)]$Meara
+uc_subset <- uc_subset[order(Meara)]
+pivot_low <-  uc_subset[nrow(uc_subset)*(1/3)]$Meara
+pivot_high <-  uc_subset[nrow(uc_subset)*(2/3)]$Meara
 combined_matrix[Meara<pivot_low, Meara3 := 'Low']
 combined_matrix[Meara>=pivot_low & Meara<pivot_high, Meara3 := 'Med']
 combined_matrix[Meara>=pivot_high, Meara3 := 'High']
@@ -185,7 +198,6 @@ test <- lmerTest::lmer(numtransfrom_Refs ~ VisLit3*group + (1 | msnv) + (1 | par
                        na.omit(combined_matrix[aoi_name=='legend']), REML = TRUE)
 means1 <- lsmeans(test, pairwise ~ VisLit3|group, adjust = 'none')
 means2 <- lsmeans(test, pairwise ~ group|VisLit3, adjust = 'none')
-means2 <- lsmeans(test, pairwise ~ VisLit3|group + group|VisLit3, adjust = 'fdr')
 means1$contrasts
 means2$contrasts
 
