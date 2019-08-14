edbde1d5570cd1e1c615382b54a7c6f4b8ff3584
KristyS <kristys@alumni.ubc.ca>
2019-08-14 11:46:48 -0700

    final 3-way split method

--- UC_group_interaction_plots.R
+++ UC_group_interaction_plots.R
@@ -33,9 +33,9 @@ setnames(adpt_bcl_raw, 1, 'part_id')
 bcl_raw <- rbind(ctrl_bcl_raw, adpt_bcl_raw)
 combined_matrix <- merge(combined_matrix, bcl_raw, all.x = TRUE)
 
-uc_subset <- combined_matrix[, .SD[1], by=part_id, .SDcols=c('Meara', 'BarChartLit_raw')]
+uc_subset <- combined_matrix[, .SD[1], by=part_id, .SDcols=c('Meara', 'BarChartLit_raw', 'BarChartLit')]
 
-# 2-way split
+###### 2-way split
 combined_matrix[Meara>=median(uc_subset$Meara), Meara2 := 'High']
 combined_matrix[Meara<median(uc_subset$Meara), Meara2 := 'Low']
 combined_matrix[, Meara2 := factor(Meara2, levels=c('Low', 'High'))]
@@ -72,7 +72,6 @@ means1 <- lsmeans(test, pairwise ~ VisLit2|group, adjust = 'none')
 means2 <- lsmeans(test, pairwise ~ group|VisLit2, adjust = 'none')
 means1$contrasts
 means2$contrasts
-
 
 # 2-way split interaction plots
 theme_update(plot.title = element_text(hjust = 0.5))
@@ -121,56 +120,37 @@ ggplot(sss, aes(x=VisLit2, y=numtransfrom_Refs, group=group)) +
   labs(x = 'Vis Literacy', y = "transitions from Refs to Legend") + 
   scale_linetype_manual(values=c("dashed",'solid'))
 
-#### reversed x and y 
-sss <- summarySE(combined_matrix[aoi_name=='Relevant_bars'], measurevar="numtransfrom_Refs", groupvars=c("Meara2","group"))
-ggplot(sss, aes(x=group, y=numtransfrom_Refs, group=Meara2)) + 
-  geom_line(aes(color=Meara2)) +
-  geom_point(aes(color=Meara2)) + 
-  geom_errorbar(aes(ymin=numtransfrom_Refs-ci*0.8, ymax=numtransfrom_Refs+ci*0.8, color=Meara2), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('Relevant_bars numtransfrom_Refs')
 
-sss <- summarySE(combined_matrix[aoi_name=='Relevant_bars'], measurevar="numtransfrom_labels", groupvars=c("VisLit2","group"))
-ggplot(sss, aes(x=group, y=numtransfrom_labels, group=VisLit2)) + 
-  geom_line(aes(color=VisLit2)) +
-  geom_point(aes(color=VisLit2)) + 
-  geom_errorbar(aes(ymin=numtransfrom_labels-ci*0.8, ymax=numtransfrom_labels+ci*0.8, color=VisLit2), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('Relevant_bars numtransfrom_labels')
+##### 3-way split
 
-sss <- summarySE(combined_matrix[aoi_name=='legend'], measurevar="numtransfrom_Non_relevant_bars", groupvars=c("VisLit2","group"))
-ggplot(sss, aes(x=group, y=numtransfrom_Non_relevant_bars, group=VisLit2)) + 
-  geom_line(aes(color=VisLit2)) +
-  geom_point(aes(color=VisLit2)) + 
-  geom_errorbar(aes(ymin=numtransfrom_Non_relevant_bars-ci*0.8, ymax=numtransfrom_Non_relevant_bars+ci*0.8, color=VisLit2), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('legend numtransfrom_Non_relevant_bars')
-
-sss <- summarySE(combined_matrix[aoi_name=='legend'], measurevar="numtransfrom_Refs", groupvars=c("VisLit2","group"))
-ggplot(sss, aes(x=group, y=numtransfrom_Refs, group=VisLit2)) + 
-  geom_line(aes(color=VisLit2)) +
-  geom_point(aes(color=VisLit2)) + 
-  geom_errorbar(aes(ymin=numtransfrom_Refs-ci*0.8, ymax=numtransfrom_Refs+ci*0.8, color=VisLit2), width=.1, position = position_dodge(width = 0.05)) +
-  ggtitle('legend numtransfrom_Refs')
-
-
-# 3-way split
-
-uc_subset <- uc_subset[order(BarChartLit_raw)]
-pivot_low <- uc_subset[nrow(uc_subset)*(1/3)]$BarChartLit_raw
-pivot_high <- uc_subset[nrow(uc_subset)*(2/3)]$BarChartLit_raw
-
-combined_matrix[BarChartLit_raw<pivot_low, VisLit3 := 'Low']
-combined_matrix[BarChartLit_raw>=pivot_low & BarChartLit_raw<pivot_high, VisLit3 := 'Med']
-combined_matrix[BarChartLit_raw>=pivot_high, VisLit3 := 'High']
-combined_matrix[, VisLit3 := factor(VisLit3, levels=c('Low', 'Med', 'High'))]
+# uc_subset <- uc_subset[order(BarChartLit_raw)]
+# pivot_low <- uc_subset[nrow(uc_subset)*(1/3)]$BarChartLit_raw
+# pivot_high <- uc_subset[nrow(uc_subset)*(2/3)]$BarChartLit_raw
+# combined_matrix[BarChartLit_raw<pivot_low, VisLit3 := 'Low']
+# combined_matrix[BarChartLit_raw>=pivot_low & BarChartLit_raw<pivot_high, VisLit3 := 'Med']
+# combined_matrix[BarChartLit_raw>=pivot_high, VisLit3 := 'High']
+# combined_matrix[, VisLit3 := factor(VisLit3, levels=c('Low', 'Med', 'High'))]
 
 uc_subset <- uc_subset[order(Meara)]
-pivot_low <-  uc_subset[nrow(uc_subset)*(1/3)]$Meara
-pivot_high <-  uc_subset[nrow(uc_subset)*(2/3)]$Meara
+pivot_low <- uc_subset[nrow(uc_subset)*(1/3)]$Meara
+pivot_high <- uc_subset[nrow(uc_subset)*(2/3)]$Meara
 combined_matrix[Meara<pivot_low, Meara3 := 'Low']
 combined_matrix[Meara>=pivot_low & Meara<pivot_high, Meara3 := 'Med']
 combined_matrix[Meara>=pivot_high, Meara3 := 'High']
 combined_matrix[, Meara3 := factor(Meara3, levels=c('Low', 'Med', 'High'))]
 
-
+# Dereck's way of 3-way split for VisLit
+uc_subset <- uc_subset[order(BarChartLit,BarChartLit_raw)]
+pivot_lowA <- uc_subset[nrow(uc_subset)*(1/3)]$BarChartLit
+pivot_highA <- uc_subset[nrow(uc_subset)*(2/3)]$BarChartLit
+pivot_lowB <- uc_subset[nrow(uc_subset)*(1/3)]$BarChartLit_raw
+pivot_highB <- uc_subset[nrow(uc_subset)*(2/3)]$BarChartLit_raw
+combined_matrix[BarChartLit<pivot_lowA, VisLit3 := 'Low']
+combined_matrix[BarChartLit==pivot_lowA & BarChartLit_raw<pivot_lowB, VisLit3 := 'Low']
+combined_matrix[BarChartLit<=pivot_highA & BarChartLit>pivot_lowA, VisLit3 := 'Med']
+combined_matrix[BarChartLit==pivot_lowA & BarChartLit_raw>=pivot_lowB, VisLit3 := 'Med']
+combined_matrix[BarChartLit>pivot_highA, VisLit3 := 'High']
+combined_matrix[, VisLit3 := factor(VisLit3, levels=c('Low', 'Med', 'High'))]
 
 # new pruned models without non-significant terms found in stage 1 and contrast tests
 test <- lmerTest::lmer(numtransfrom_Refs ~ Meara3*group + (1 | msnv) + (1 | part_id),
@@ -200,7 +180,6 @@ means1 <- lsmeans(test, pairwise ~ VisLit3|group, adjust = 'none')
 means2 <- lsmeans(test, pairwise ~ group|VisLit3, adjust = 'none')
 means1$contrasts
 means2$contrasts
-
 
 # 3-way split interaction plots
 
